a8be66d7f0f0712f67024ee4d8d55e6e96bbc44f
TheSerpentCoder <himmu1144@gmail.com>
2024-07-28 02:09:18 +0530

    DSA Practise/CheatSheet

---
+++ 84_Graphs.py
@@ -0,0 +1,11 @@
+"""
+Representation of Graphs 
+
+1. Adjacency List - Mark the nodes with the list of its neighbors 
+
+2. Adjaceny Matrix - Aij = 1 for an edge between i and j otherwise 0
+
+Some other representations are Edge set , Cost Adjacency List , Compact list representation etc...
+    
+
+"""
